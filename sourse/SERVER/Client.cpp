bed9ceb3d104b3ae0739cb41f73051e761b52f6a
filka657 <72990504+filka657@users.noreply.github.com>
2022-04-30 10:53:48 +0300

    Update Client.cpp

--- sourse/SERVER/Client.cpp
+++ sourse/SERVER/Client.cpp
@@ -26,7 +26,7 @@ int __cdecl main(int argc, char** argv)
     const char* sendbuf = "this is a test";
 
 
-    char arr[50] = "AAAAA";
+    char arr[10] = "AAAAA";
 
 
     char recvbuf[DEFAULT_BUFLEN];
@@ -90,7 +90,7 @@ int __cdecl main(int argc, char** argv)
     }
 
     // Send an initial buffer
-    iResult = send(ConnectSocket, arr, 50, 0);
+    iResult = send(ConnectSocket, arr, 10, 0);
     if (iResult == SOCKET_ERROR) {
         printf("send failed with error: %d\n", WSAGetLastError());
         closesocket(ConnectSocket);
@@ -112,7 +112,7 @@ int __cdecl main(int argc, char** argv)
     // Receive until the peer closes the connection
     do {
 
-        iResult = recv(ConnectSocket, arr, 50, 0);
+        iResult = recv(ConnectSocket, arr, 10, 0);
         if (iResult > 0)
             printf("Bytes received: %d\n", iResult);
         else if (iResult == 0)
